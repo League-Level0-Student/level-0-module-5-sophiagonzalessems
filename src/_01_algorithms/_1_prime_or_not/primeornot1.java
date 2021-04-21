61ae4d88ab33c4fe2057c37bb22fd0a6545061a7
student <student@ip-172-31-63-154.us-west-2.compute.internal>
2021-04-21 16:26:22 -0700

    finish

---
+++ src/_01_algorithms/_1_prime_or_not/primeornot1.java
@@ -0,0 +1,28 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class primeornot1 {
+
+
+public static void main(String[] args) {
+
+	String prime=JOptionPane.showInputDialog("Choose a number.");
+	if(test.equals)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
