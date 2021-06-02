902a0c439849111dd2978777b1662cc7db5cea7e
student <student@ip-172-31-53-55.us-west-2.compute.internal>
2021-06-02 16:26:28 -0700

    finish

--- src/_01_algorithms/_2_fibonacci/algorthms.java
+++ src/_01_algorithms/_2_fibonacci/algorthms.java
@@ -1,6 +1,5 @@
 package _01_algorithms._2_fibonacci;
 
-import com.sun.tools.javac.launcher.Main;
 
 public class algorthms {
 
@@ -11,7 +10,14 @@ public class algorthms {
 		int sum=num1+num2;
 		//0,1,1,2,3,5,8,13,21//
 		
-		println(num1)
+		System.out.println(num1+" "+num2);
+		System.out.println(sum);
+		
+		for(int i)
+		
+		
+		
+		
 		
 		
 				
